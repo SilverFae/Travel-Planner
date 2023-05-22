cbeb82476098ccee7e2d052d832d3b2e3751d087
Shannon <shogeboom187@gmail.com>
2023-05-21 20:08:42 -0700

    finished index.js through models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Catagory.hasMany(Product, {
+ foreignKey: 'product_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag,{
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag,{
+  through: {
+    foreignKey: 'tag_id'
+  }
+});
 
 module.exports = {
   Product,
